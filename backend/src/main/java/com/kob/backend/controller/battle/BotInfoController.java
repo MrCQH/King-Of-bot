17e02be2c4a0c77c76d63864c0e48b4e133f6505
qh c <1136156543@qq.com>
2022-07-30 15:38:20 +0800

    配置Mysql与注册登录模块（上）

--- backend/src/main/java/com/kob/backend/controller/battle/BotInfoController.java
+++ backend/src/main/java/com/kob/backend/controller/battle/BotInfoController.java
@@ -1,17 +1,13 @@
-package com.kob.backend.controller.pk;
+package com.kob.backend.controller.battle;
 
-import com.sun.glass.ui.ClipboardAssistance;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import sun.net.www.content.text.PlainTextInputStream;
 
 import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/pk/")
+@RequestMapping("/battle/")
 public class BotInfoController {
 
     @RequestMapping("getbotinfo/")
